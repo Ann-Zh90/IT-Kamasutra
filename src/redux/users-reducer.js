cc6279c951754e22a239035a6e042d8994def850
AnnaZh <anna.s.zhyla@gmail.com>
2021-08-30 12:57:32 +0300

    created 2nd container component UserContainer_Lesson 56

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,9 +1,15 @@
 const FOLLOW = 'FOLlOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET-USERS';
+const SET_CURRENT_PAGE = 'SET-CURRENT-PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET-TOTAL-USERS-COUNT';
+
 
 let initialState = {
     users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1,
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -31,6 +37,13 @@ const usersReducer = (state = initialState, action) => {
         case SET_USERS: {
             return {...state, users: [...action.users]}
         }
+        case SET_CURRENT_PAGE: {
+            return {...state, currentPage: action.currentPage}
+        }
+        case SET_TOTAL_USERS_COUNT: {
+            return {...state, totalUsersCount: action.count}
+        }
+
         default:
             return state;
     }
@@ -40,5 +53,7 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userID) => ({type: FOLLOW, userID});
 export const unfollowAC = (userID) => ({type: UNFOLLOW, userID})
 export const setUsersAC = (users) => ({type: SET_USERS, users})
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
+export const setUsersTotalCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount})
 
 export default usersReducer;
