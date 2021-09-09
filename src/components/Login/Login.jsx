4a5ccada5fd657e00f6b7793314d2c18e8f57667
AnnaZh <anna.s.zhyla@gmail.com>
2021-09-09 11:46:22 +0300

    installed redux-form and used it on Login_Lesson 75

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import {Field, reduxForm} from "redux-form";
+
+
+const LoginForm = (props) => {
+    return <form onSubmit={props.handleSubmit}>
+        <div>
+            <Field placeholder={'Login'} name={'login'} component={'input'}/>
+        </div>
+        <div>
+            <Field placeholder={'Password'} name={'password'}component={'input'}/>
+        </div>
+        <div>
+            <Field type={'checkbox'} name={'rememberMe'} component={'input'}/> remember me
+        </div>
+        <div>
+            <button>Login</button>
+        </div>
+    </form>
+}
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
 
 const Login = (props) => {
-    return <h1>Login</h1>
+    const haha =(qw) => {
+        console.log(qw)
+    }
+
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={haha} some={'nghm'}/>
+    </div>
 }
 
 export default Login
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,5 +1,4 @@
 import {profileAPI, usersAPI} from "../api/api";
-import {resetFirstInputPolyfill} from "web-vitals/dist/modules/lib/polyfills/firstInputPolyfill";
 
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
